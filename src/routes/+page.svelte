fb980c4cf65297ee1c35a14b24e5babba509f601
chientrm <chientrm@gmail.com>
2023-07-01 02:23:08 +0000

    reproduce

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { base } from "$app/paths";
+</script>
+
+<h1>Home</h1>
+
+<a href={`${base}/foo`}>Foo</a>
---
+++ src/routes/foo/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+  import { base } from "$app/paths";
+</script>
+
+<h1>Foo</h1>
+<a href={`${base}/`}>Back</a>
